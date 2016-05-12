1c31bd251d1ccac6bf45a7ed8ef9f87486b9707c
cklynch2 <cklynch2@gmail.com>
2016-05-12 18:00:58 -0400

    Fibonacci sequence is everywhere in nature and now on my computer too woot woot

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -9,10 +9,28 @@
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+-(NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index
+{
+    NSMutableArray *sequence = [[NSMutableArray alloc]init];
+    for (NSUInteger i = 0; i < index + 1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        }
+        else if (i == 1) {
+            [sequence addObject:@1];
+        }
+        else {
+            NSNumber *iMinusOne = sequence[i-1];
+            NSNumber *iMinusTwo = sequence[i-2];
+            NSUInteger iMinusOneInt = [iMinusOne integerValue];
+            NSUInteger iMinusTwoInt = [iMinusTwo integerValue];
+            NSUInteger nextInt = iMinusOneInt + iMinusTwoInt;
+            NSNumber *next = @(nextInt);
+            [sequence addObject:next];
+        }
+    }
+    NSLog(@"%@", sequence);
+    return sequence;
+}
 
 @end
